fd1562f0dbef7a07778f9d5b5e39febab15a33b4
Louis Lombard <louis.raphael.lombard@gmail.com>
2021-05-05 15:57:13 +0200

    Make it better

--- source/datamanipulation.cpp
+++ source/datamanipulation.cpp
@@ -79,9 +79,11 @@ Catalogue::Catalogue ()
     {
         while ( getline (myfile,line) )
         {
+            
         cout << line << '\n';
         }
         myfile.close();
+        
     }else cout << "Unable to open file"; 
     
   	
