42af5fb6cc44d97f9c4fcfd48a51853130e67b97
Kinepo <alexandre.senouci@insa-lyon.fr>
2021-05-05 16:22:02 +0200

    stable

---
+++ interface/Measure.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexandre Senouci on 05/05/2021.
+//
+
+#ifndef IF_3_GL_PROJECT_MEASURE_H
+#define IF_3_GL_PROJECT_MEASURE_H
+
+#endif //IF_3_GL_PROJECT_MEASURE_H
---
+++ source/Measure.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexandre Senouci on 05/05/2021.
+//
+
+#include "../interface/Measure.h"
