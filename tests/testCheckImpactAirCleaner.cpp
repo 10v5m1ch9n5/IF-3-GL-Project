17d1db8cf58eb4ddf25e5abf9ceea37e7fe9fe1f
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2021-05-30 17:46:57 +0200

    Test de l'impact d'un AirCleaner fonctionne

--- tests/testCheckImpactAirCleaner.cpp
+++ tests/testCheckImpactAirCleaner.cpp
@@ -7,7 +7,13 @@
 int main()
 {
     DataManipulation dm;
-    pair<int,int> evol = dm.checkImpactAirCleaner("1", 0.6);
+    // Cleaner 1
+    // Capteurs dans un rayon de 0.6 : sensor66;sensor67;sensor76;sensor77;sensor87
+    // Mise en route : 01/02/10
+    // Indice ATMO moyen des capteurs le 31/01/19 : 3,4
+    // Arret du AirCleaner : 01/03/19
+    // Indice ATMO moyen des capteurs le 28/02/19 : 1
+    pair<int,int> evol = dm.checkImpactAirCleaner("Cleaner1", 0.6);
     cout << evol.first << " " << evol.second << endl;
 
     return 0;
